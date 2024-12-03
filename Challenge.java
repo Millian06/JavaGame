ef6c0893b9848b573537fff689330ca01d796b80
Millian06 <m.martey@rgu.ac.uk>
2024-12-03 10:04:32 +0000

    Challenge.java

--- Challenge.java
+++ Challenge.java
@@ -1,68 +1,86 @@
 import java.util.Scanner;
 
-public abstract Challenge{
-protected String description;
-public Challenge(String description){
-    this.description = description;
+public abstract class Challenge {
+    protected String description;
 
-}
+    public Challenge(String description) {
+        this.description = description;
+    }
 
-public abstract boolean attempt{
-    class Puzzle extends Challenge{
+    public abstract boolean attempt();
+
+    public static class Puzzle extends Challenge {
         private String question;
         private String answer;
 
-        public Puzzle(String description, String question, String answer){
+        public Puzzle(String description, String question, String answer) {
             super(description);
             this.question = question;
             this.answer = answer;
-
-
         }
 
         @Override
         public boolean attempt() {
             System.out.println(description);
-            System.out.println("Puzzle" + question);
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Input Answer: ");
-            String response = scanner.nextLine();
-            return response.equalIgnoreCase(answer);
-
+            System.out.println("Puzzle: " + question);
+            try (Scanner scanner = new Scanner(System.in)) {
+                System.out.print("Input Answer: ");
+                String response = scanner.nextLine();
+                return response.equalsIgnoreCase(answer);
+            }
         }
     }
-class Enemy extends Challenge{
-    private int health;
-    private int attackHit;
 
-    public Enemy(String description, int health, int attackHit){
-        super(description);
-        this.health = health;
-        this.attackHit = attackHit;
-    }
+    public static class Enemy extends Challenge {
+        private int health;
+        @SuppressWarnings("unused")
+        private int attackHit;
 
-    @Override
+        public Enemy(String description, int health, int attackHit) {
+            super(description);
+            this.health = health;
+            this.attackHit = attackHit;
+        }
 
-    public boolean attempt{
-        Scanner scanner = new Scanner(System.in);
-        while (health > 0){
-            System.out.println(description + "Player Health: " + health);
-            System.out.println("Type attack to strike");
-            String action = scanner.nextLine();
-            if (action.equalsIgnoreCase("strike")){
-                int damage = newRandom().nextInt(10) + 5;
-
-                health -= damage;
-                System.out.println("You received " + damage + "damage!!!");
-            }else{
-                System.out.println("Invalid Action!");
-
+        @Override
+        public boolean attempt(Player player) {
+            Scanner scanner = new Scanner(System.in);
+            java.util.Random random = new java.util.Random();
+        
+            while (health > 0 && player.isAlive()) {
+                System.out.println(description + " Enemy Health: " + health);
+                System.out.println("Type 'strike' to attack.");
+                String action = scanner.nextLine();
+                if (action.equalsIgnoreCase("strike")) {
+                    int damage = random.nextInt(10) + 5;
+                    health -= damage;
+                    System.out.println("You dealt " + damage + " damage!");
+        
+                    if (health > 0) {
+                        int enemyDamage = attackHit;
+                        player.takeDamage(enemyDamage);
+                        if (!player.isAlive()) {
+                            System.out.println("You were defeated by the enemy!");
+                            return false;
+                        }
+                    }
+                } else {
+                    System.out.println("Invalid action!");
+                }
             }
-            }
-                System.out.println("You defeated the enemy!");
-                return true;
-    }       
-}
-}
+        
+            System.out.println("You defeated the enemy!");
+            return true;
+        }
 
-}
+        @Override
+        public boolean attempt() {
+            throw new UnsupportedOperationException("Unimplemented method 'attempt'");
+        }
+        
+                }
+            }
+
+            System.out.println("You defeated the enemy!") ;
+            return true;
+        
